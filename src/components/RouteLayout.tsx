13cc8d2e47e6046d6fc970d0e9fda3d0089dfa69
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-05-29 11:31:05 +0700

    fix: more layoutforAdmin

--- src/components/RouteLayout.tsx
+++ src/components/RouteLayout.tsx
@@ -2,10 +2,15 @@ import SideNavbar from '@/components/global/organisms/SideNavbar'
 import { Outlet } from 'react-router-dom'
 function RouteLayout() {
   return (
-    <div className='min-h-screen w-full bg-white text-black flex '>
-      <SideNavbar />
-      <div className='p-8 w-full'>
-        <Outlet />
+    <div>
+        <div>fhgkhfkghll;jgljfl
+            jkhjghj
+        </div>
+      <div className='min-h-screen w-full bg-white text-black flex '>
+        <SideNavbar />
+        <div className='p-8 w-full'>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
--- src/components/global/atoms/ui/nav.tsx
+++ src/components/global/atoms/ui/nav.tsx
@@ -38,7 +38,7 @@ const pathName = location.pathname;
         data-collapsed={isCollapsed}
         className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2"
       >
-        <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+        <nav className="grid pr-8 gap-2 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
           {links.map((link, index) =>
             isCollapsed ? (
               <Tooltip key={index} delayDuration={0}>
@@ -50,12 +50,12 @@ const pathName = location.pathname;
                         variant: link.href === pathName ? "default" : "ghost",
                         size: "icon"
                       }),
-                      "h-9 w-9",
+                      "h-10 w-10",
                       link.variant === "default" &&
                         "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
                     )}
                   >
-                    <link.icon className="h-4 w-4" />
+                    <link.icon className="h-5 w-5" />
                     <span className="sr-only">{link.title}</span>
                   </Link>
                 </TooltipTrigger>
@@ -78,14 +78,14 @@ const pathName = location.pathname;
                 className={cn(
                   buttonVariants({
                     variant: link.href === pathName ? "default" : "ghost",
-                    size: "sm"
+                    size: "lg"
                   }),
                   link.variant === "default" &&
                     "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
                   "justify-start"
                 )}
               >
-                <link.icon className="mr-2 h-4 w-4" />
+                <link.icon className="mr-2 h-5 w-5" />
                 {link.title}
                 {link.label && (
                   <span
